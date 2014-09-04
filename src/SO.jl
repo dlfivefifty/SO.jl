42185458d83c8aad008bd4e5b38646dc311831a6
Sheehan Olver <solver@mac.com>
2014-09-04 12:14:21 +1000

    added file, removed pyplot

---
+++ src/SO.jl
@@ -0,0 +1,49 @@
+module SO
+    using Base
+    
+export dotplot, tomovie, chopm
+
+chopm(x::Complex,tol)=abs(imag(x)) < tol ? chopm(real(x)) : chopm(real(x)) + chopm(imag(x))im
+chopm(x::Real,tol)=abs(x-round(x)) < tol ? int(round(x)) : x
+chopm(x,tol)=x
+
+chopm(A::Array,tol)=Any[chopm(A[k,j],tol) for k=1:size(A,1), j=1:size(A,2)]
+
+chopm(B::SparseMatrixCSC,tol)=chopm(full(B),tol)
+
+
+
+chopm(x)=chopm(x,1000eps())
+
+
+
+
+##Plot
+
+dotplot(r,opts...)=PyPlot.scatter(real(r),imag(r),opts...)
+
+function tomovie(v::Array;plotfunction=plot,zlims=-1,opts...)
+    tm=string(time_ns()) 
+    dr="/tmp/" * tm * "mov"
+
+    mkdir(dr)
+
+    for k=1:size(v,1)
+        for j=1:size(v,2)
+            if zlims!=-1
+                PyPlot.zlim(zlims[1],zlims[2])                    
+            end
+            plotfunction(v[k,j];opts...)        
+        end
+        PyPlot.savefig(dr * "/" * string(k) * ".png")    
+        PyPlot.clf()
+    end
+
+    run(`ffmpeg -r 25 -i $dr/%d.png $dr/out.mpg`)
+    run(`open $dr/out.mpg`)
+end
+
+
+ENV["COLUMNS"]=131
+
+end #module
