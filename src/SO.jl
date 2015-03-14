f1282ee85db0c110ec5b0648bc88f15221f03eb6
Sheehan Olver <solver@mac.com>
2015-03-14 11:50:28 +1100

    add export/importmathematica

--- src/SO.jl
+++ src/SO.jl
@@ -1,7 +1,7 @@
 module SO
     using Base
-    
-export dotplot, tomovie, chopm
+
+export dotplot, tomovie, chopm,  exportmathematica
 
 chopm(x::Complex,tol)=abs(imag(x)) < tol ? chopm(real(x)) : chopm(real(x)) + chopm(imag(x))im
 chopm(x::Real,tol)=abs(x-round(x)) < tol ? int(round(x)) : x
@@ -23,7 +23,7 @@ chopm(x)=chopm(x,1000eps())
 dotplot(r,opts...)=Main.PyPlot.scatter(real(r),imag(r),opts...)
 
 function tomovie(v::Array;plotfunction=plot,zlims=-1,opts...)
-    tm=string(time_ns()) 
+    tm=string(time_ns())
     dr="/tmp/" * tm * "mov"
 
     mkdir(dr)
@@ -31,17 +31,23 @@ function tomovie(v::Array;plotfunction=plot,zlims=-1,opts...)
     for k=1:size(v,1)
         for j=1:size(v,2)
             if zlims!=-1
-                PyPlot.zlim(zlims[1],zlims[2])                    
+                PyPlot.zlim(zlims[1],zlims[2])
             end
-            plotfunction(v[k,j];opts...)        
+            plotfunction(v[k,j];opts...)
         end
-        PyPlot.savefig(dr * "/" * string(k) * ".png")    
+        PyPlot.savefig(dr * "/" * string(k) * ".png")
         PyPlot.clf()
     end
 
     run(`ffmpeg -r 25 -i $dr/%d.png $dr/out.mpg`)
     run(`open $dr/out.mpg`)
 end
+
+
+## Marthematica
+
+exportmathematica(A::Array)=writecsv("/tmp/juliatowm.csv",A)
+importmathematica()=readcsv("/tmp/juliatowm.csv")
 
 
 ENV["COLUMNS"]=130
