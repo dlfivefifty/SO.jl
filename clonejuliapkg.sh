97c21a57f5142c432a6f7cd08fce483fbfa7322c
Sheehan Olver <solver@mac.com>
2018-06-05 16:06:29 +0100

    0.7

--- clonejuliapkg.sh
+++ clonejuliapkg.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-if [ ! -d $HOME/Projects/$2 ]; then
+if [ ! -d $HOME/Projects/$2.jl ]; then
 	git clone https://github.com/$1/$2.jl $HOME/Projects/$2.jl
 fi
-if [ -d $HOME/.julia/v$JULIA_VERSION/$2 ]; then
-	rm -rf $HOME/.julia/v$JULIA_VERSION/$2
+if [ -d $HOME/.julia/v0.6/$2 ]; then
+	rm -rf $HOME/.julia/v0.6/$2
 fi
-ln -s $HOME/Projects/$2.jl $HOME/.julia/v$JULIA_VERSION/$2
+if [ -d $HOME/.julia/dev/$2 ]; then
+rm -rf $HOME/.julia/dev/$2
+fi
+ln -s $HOME/Projects/$2.jl $HOME/.julia/v0.6/$2
+ln -s $HOME/Projects/$2.jl $HOME/.julia/dev/$2
