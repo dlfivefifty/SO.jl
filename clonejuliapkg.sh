fa2740e378f58dc130f70a334cf967cfd827a73f
Sheehan Olver <solver@mac.com>
2018-06-12 19:45:52 +0100

    Pkg3 develop

--- clonejuliapkg.sh
+++ clonejuliapkg.sh
@@ -6,8 +6,5 @@ fi
 if [ -d $HOME/.julia/v0.6/$2 ]; then
 	rm -rf $HOME/.julia/v0.6/$2
 fi
-if [ -d $HOME/.julia/dev/$2 ]; then
-rm -rf $HOME/.julia/dev/$2
-fi
 ln -s $HOME/Projects/$2.jl $HOME/.julia/v0.6/$2
-ln -s $HOME/Projects/$2.jl $HOME/.julia/dev/$2
+$HOME/Projects/julia7/julia -e "Pkg.develop(\"$HOME/Projects/$2.jl\")"
