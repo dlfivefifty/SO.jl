1de2f92178f9d3a6c07c11a256acc9e731aa12eb
Sheehan Olver <solver@mac.com>
2018-08-23 22:19:10 +0100

    update develop syntax

--- clonejuliapkg.sh
+++ clonejuliapkg.sh
@@ -10,5 +10,5 @@ ln -s $HOME/Projects/$2.jl $HOME/.julia/v0.6/$2
 
 
 
-/Applications/Julia-0.7.app/Contents/Resources/julia/bin/julia -e "Pkg.develop(\"$HOME/Projects/$2.jl\")"
-/Applications/Julia-1.0.app/Contents/Resources/julia/bin/julia -e "Pkg.develop(\"$HOME/Projects/$2.jl\")"
+/Applications/Julia-0.7.app/Contents/Resources/julia/bin/julia -e "using Pkg; Pkg.develop(PackageSpec(url=\"$HOME/Projects/$2.jl\"))"
+/Applications/Julia-1.0.app/Contents/Resources/julia/bin/julia -e "using Pkg; Pkg.develop(PackageSpec(url=\"$HOME/Projects/$2.jl\"))"
