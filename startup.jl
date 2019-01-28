bd912ef01c1f43d08a7b29773f0e337b86a6aa56
Sheehan Olver <solver@mac.com>
2019-01-28 15:59:22 +0000

    startup

---
+++ startup.jl
@@ -0,0 +1,20 @@
+try # avoid bug in building packages
+	using LinearAlgebra, Pkg, SparseArrays, Random, Test
+catch ex
+	println("$ex thrown during startup")
+end
+
+try # avoid bug in building packages
+	using SpecialFunctions
+catch ex
+	println("$ex thrown during startup")
+end
+
+try # avoid bug in building packages
+	isfile("Project.toml") && Pkg.activate(".") # auto-activate Project in Juno
+catch ex
+	println("$ex thrown during startup")
+end
+
+
+linspace(a,b,n) = range(a; stop=b, length=n)
